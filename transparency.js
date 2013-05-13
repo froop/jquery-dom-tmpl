b3be44c9dedd29202b07b0f7451e4ae1c2aa15de
froop <frooprogrammer@gmail.com>
2013-05-13 17:01:55 +0900

    modify transparency.js add Radio.prototype.render()

--- transparency.js
+++ transparency.js
@@ -490,6 +490,10 @@ Radio = (function(_super) {
 
   ElementFactory.Elements['input']['radio'] = Radio;
 
+  Radio.prototype.render = function(value) {
+    return this.attr('checked', this.el.value === String(value));
+  };
+
   return Radio;
 
 })(Checkbox);
