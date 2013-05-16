7bea934b77a061c5bbb9a206c8a59a5462ae8053
froop <frooprogrammer@gmail.com>
2013-05-17 06:48:26 +0900

    add test.html frame

---
+++ test/test.js
@@ -0,0 +1,14 @@
+(function ($) {
+	"use strict";
+
+	module("domtmpl", {
+		setup: function () {
+//			$("#qunit-fixture").append(
+//					$("<div>").html(html).find("#example"));
+		}
+	});
+
+	test("dummy", function () {
+		ok(true);
+	});
+})(jQuery);
