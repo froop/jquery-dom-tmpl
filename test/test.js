855446e21cfe6e86a4d2fbf7d0c078d883469990
froop <frooprogrammer@gmail.com>
2013-06-05 15:34:57 +0900

    add test tmplUnbind

--- test/test.js
+++ test/test.js
@@ -26,4 +26,59 @@
 		ok($("#custom2 > input").prop("disabled"));
 		equal($("#nest11").text(), "nested text");
 	});
+
+	test("tmplUnbind", function () {
+		function deleteComma(str) {
+			return new String(str).replace(/,/g, "");
+		}
+		var res = $("#direct-bind").tmplUnbind({
+			text1: "",
+			text2: "",
+			input1: "",
+			input2: 0,
+			checkbox1: false,
+			textarea1: "",
+			select1: "",
+			radio1: "",
+			checkbox2: [],
+			alternate1: "",
+			alternate21: "",
+			alternate22: "",
+			nest1: {
+				nest11: ""
+			}
+		}, {
+			find: {
+				alternate1: "[name=custom12]",
+				alternate21: "#custom21",
+				alternate22: "#custom2 > input"
+			},
+			convertCallback: function (value, name) {
+				if (name === "input2") {
+					return deleteComma(value);
+				} else {
+					return value;
+				}
+			},
+			error: true
+		});
+
+		deepEqual(res, {
+			text1: "root element",
+			text2: "child element",
+			input1: "input text",
+			input2: 1234,
+			checkbox1: true,
+			textarea1: "textarea11\ntextarea12",
+			select1: "s1",
+			radio1: "r1",
+			checkbox2: ["c1", "c3"],
+			alternate1: "alt1",
+			alternate21: "alt21",
+			alternate22: "alt22",
+			nest1: {
+				nest11: "nested text"
+			}
+		});
+	});
 })(jQuery);
